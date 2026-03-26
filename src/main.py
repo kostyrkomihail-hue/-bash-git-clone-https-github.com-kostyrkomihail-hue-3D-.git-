cb1a09fb4caef1aeaed341ea58d0f5e2052a7746
kostyrkomihail-hue <kostyrkomihail@gmail.com>
2026-03-26 13:29:47 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -25,3 +25,11 @@ pycache/
      *.pyc
      .DS_Store
      venv/
+MIT License
+
+   Copyright (c) 2026 [Твоє ім’я]
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction...
+   `  
